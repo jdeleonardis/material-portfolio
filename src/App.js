4311ffb2b0d414f0ef047ba06b45cdb526620653
jdeleonardis <jdeleonardis@hotmail.com>
2020-12-24 13:45:50 -0500

    Added left nav, other styling

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Home from "./pages/Home/Home.js";
+import "./App.css"
+// import NavComponent from "./components/NavComponent/NavComponent.js";
+// import Footer from "./components/Footer/Footer.js";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div>
+{/*         <NavComponent/> */}
+        <Switch>
+            <Route exact path="/" component={Home} />
+            {/* <Route exact path="/classes" component={Classes} />
+            <Route exact path="/resources" component={Resources} />            
+            <Route exact path="/contact" component={Contact} />                    
+            <Route exact path="/login" component={Login} />                              
+            <Route exact path="/adminhome" component={withAuth(AdminHome)} /> */}
+        </Switch>
+        {/* <Footer/> */}
+      </div>
+    </BrowserRouter>
+  );
 }
+export default App;
 
-export default App;
---
+++ src/pages/Home/Home.js
@@ -0,0 +1,250 @@
+import React from 'react';
+import clsx from 'clsx';
+import { withStyles, makeStyles, useTheme } from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import List from '@material-ui/core/List';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Typography from '@material-ui/core/Typography';
+import Divider from '@material-ui/core/Divider';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import HouseIcon from '@material-ui/icons/House';
+import FaceIcon from '@material-ui/icons/Face';
+import ContactMailIcon from '@material-ui/icons/ContactMail';
+import MenuBookIcon from '@material-ui/icons/MenuBook';
+import Tooltip from '@material-ui/core/Tooltip';
+
+const drawerWidth = 170;
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+  },
+  appBar: {
+    zIndex: theme.zIndex.drawer + 1,
+    // height: '50px',
+    backgroundColor:  'rgba(9,17,52,1)',
+    transition: theme.transitions.create(['width', 'margin'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+  },
+  appBarShift: {
+    marginLeft: drawerWidth,
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(['width', 'margin'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  menuButton: {
+    marginRight: 36,
+  },
+  hide: {
+    display: 'none',
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+    whiteSpace: 'nowrap'    
+  },
+  drawerOpen: {
+    width: drawerWidth,
+    transition: theme.transitions.create('width', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+    backgroundColor: 'rgba(9,17,52,1)'
+    
+  },
+  drawerClose: {
+    transition: theme.transitions.create('width', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    overflowX: 'hidden',
+    width: theme.spacing(7) + 1,
+    [theme.breakpoints.up('sm')]: {
+      width: theme.spacing(9) + 1,
+    },
+    backgroundColor: 'rgba(9,17,52,1)'    
+  },
+  toolbar: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    padding: theme.spacing(0, 1),
+    // necessary for content to be below app bar
+    ...theme.mixins.toolbar,
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
+  },
+  icon: {
+      color: 'white'
+  }
+}));
+
+
+const HtmlTooltip = withStyles((theme) => ({
+    tooltip: {
+      backgroundColor: '#f5f5f9',
+      color: 'rgba(0, 0, 0, 0.87)',
+      maxWidth: 220,
+      fontSize: theme.typography.pxToRem(12),
+      border: '1px solid #dadde9',
+    },
+  }))(Tooltip);
+
+export default function Home() {
+  const classes = useStyles();
+  const theme = useTheme();
+  const [open, setOpen] = React.useState(false);
+  const [selectedIndex, setSelectedIndex] = React.useState(0);
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
+  const clickHandler = (event, text, index) => {
+    //   alert(text)
+      setSelectedIndex(index)
+  }
+
+  const iconSwitch = (icon) => {
+    switch(icon) {
+        case 'Home':
+            return <HouseIcon className={classes.icon}/>
+        case 'About':
+            return <FaceIcon className={classes.icon}/>            
+        case 'Portfolio':
+            return <MenuBookIcon className={classes.icon}/>                
+        case 'Contact':
+            return <ContactMailIcon className={classes.icon}/>
+        default:
+            return <></>                                
+
+
+    }
+  }
+
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+      <AppBar
+        position="fixed"
+        className={clsx(classes.appBar, {
+          [classes.appBarShift]: open,
+        })}
+      >
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            onClick={handleDrawerOpen}
+            edge="start"
+            className={clsx(classes.menuButton, {
+              [classes.hide]: open,
+            })}
+          >
+            <MenuIcon 
+            // htmlColor='red'
+            />
+          </IconButton>
+          <Typography variant="h6" noWrap>
+            James L DeLeonardis
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        variant="permanent"
+        className={clsx(classes.drawer, {
+          [classes.drawerOpen]: open,
+          [classes.drawerClose]: !open,
+        })}
+        classes={{
+          paper: clsx({
+            [classes.drawerOpen]: open,
+            [classes.drawerClose]: !open,
+          }),
+        }}
+      >
+        <div className={classes.toolbar}>
+          <IconButton onClick={handleDrawerClose}>
+            {theme.direction === 'rtl' ? <ChevronRightIcon htmlColor='white'/> : <ChevronLeftIcon htmlColor='white'/>}
+          </IconButton>
+        </div>
+        <Divider />
+        <List>
+          {['Home', 'About', 'Portfolio', 'Contact'].map((text, index) => (
+            
+            //see HtmlToolTip declaration above
+            <HtmlTooltip     
+            title={
+              <React.Fragment>
+                <Typography color="inherit">Tooltip with HTML</Typography>
+                <em>{"And here's"}</em> <b>{'some'}</b> <u>{'amazing content'}</u>.{' '}
+                {"It's very engaging. Right?"} {text}
+              </React.Fragment>
+            }
+            enterDelay={600}
+            enterNextDelay={600} 
+          >
+
+            <ListItem 
+                button
+                key={text}
+                // onClick={() => setCollapsed(!collapsed)}>
+                onClick={(event) => clickHandler(event, text, index)}
+                selected={selectedIndex === index}
+                >
+                <ListItemIcon>{iconSwitch(text)}</ListItemIcon>             
+                <ListItemText primary={text} className={classes.icon}/>
+            </ListItem>
+
+            </HtmlTooltip>
+          ))}
+        </List>
+        {/* <Divider /> */}
+      </Drawer>
+      <main className={classes.content}>
+        <div className={classes.toolbar} />
+        <Typography paragraph>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+          ut labore et dolore magna aliqua. Rhoncus dolor purus non enim praesent elementum
+          facilisis leo vel. Risus at ultrices mi tempus imperdiet. Semper risus in hendrerit
+          gravida rutrum quisque non tellus. Convallis convallis tellus id interdum velit laoreet id
+          donec ultrices. Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit
+          adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra nibh cras.
+          Metus vulputate eu scelerisque felis imperdiet proin fermentum leo. Mauris commodo quis
+          imperdiet massa tincidunt. Cras tincidunt lobortis feugiat vivamus at augue. At augue eget
+          arcu dictum varius duis at consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem
+          donec massa sapien faucibus et molestie ac.
+        </Typography>
+        <Typography paragraph>
+          Consequat mauris nunc congue nisi vitae suscipit. Fringilla est ullamcorper eget nulla
+          facilisi etiam dignissim diam. Pulvinar elementum integer enim neque volutpat ac
+          tincidunt. Ornare suspendisse sed nisi lacus sed viverra tellus. Purus sit amet volutpat
+          consequat mauris. Elementum eu facilisis sed odio morbi. Euismod lacinia at quis risus sed
+          vulputate odio. Morbi tincidunt ornare massa eget egestas purus viverra accumsan in. In
+          hendrerit gravida rutrum quisque non tellus orci ac. Pellentesque nec nam aliquam sem et
+          tortor. Habitant morbi tristique senectus et. Adipiscing elit duis tristique sollicitudin
+          nibh sit. Ornare aenean euismod elementum nisi quis eleifend. Commodo viverra maecenas
+          accumsan lacus vel facilisis. Nulla posuere sollicitudin aliquam ultrices sagittis orci a.
+        </Typography>
+      </main>
+    </div>
+  );
+}
