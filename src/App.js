c983c3a69067c8cedb5fac4e5c2b953873b15f81
jdeleonardis <jdeleonardis@hotmail.com>
2021-01-05 14:54:42 -0500

    clean up

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ import Contact from "./pages/Contact/Contact.js";
 import Fab from './components/Fab/Fab'
 import "./App.css"
 import { BrowserRouter, Route, Switch, Redirect} from "react-router-dom";
-//test
 
 function App() {
 
